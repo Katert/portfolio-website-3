f9504a8b9d0f0de9b5dee7843b50c6d871956074
Wesley Kater <wesley.kater@icloud.com>
2020-10-21 23:44:25 +0200

    Updates

---
+++ precache-manifest.3b360691f57466272de1298689b1608d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c30d5923f26e735bf071b0cb6f06cc30",
+    "url": "/portfolio-website-3/index.html"
+  },
+  {
+    "revision": "4751e51bf7af7586429e",
+    "url": "/portfolio-website-3/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "bd16996f8c5d031a8ec2",
+    "url": "/portfolio-website-3/static/css/main.88a6c3c5.chunk.css"
+  },
+  {
+    "revision": "4751e51bf7af7586429e",
+    "url": "/portfolio-website-3/static/js/2.9e5f28c2.chunk.js"
+  },
+  {
+    "revision": "b3e2770e6336acd821638b70a2a55e3e",
+    "url": "/portfolio-website-3/static/js/2.9e5f28c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd16996f8c5d031a8ec2",
+    "url": "/portfolio-website-3/static/js/main.1fa4e36a.chunk.js"
+  },
+  {
+    "revision": "8650578d9614134be09a",
+    "url": "/portfolio-website-3/static/js/runtime-main.0a841d74.js"
+  },
+  {
+    "revision": "84cc014157b8ddb2c14be7125a4f8a6e",
+    "url": "/portfolio-website-3/static/media/About3.84cc0141.svg"
+  },
+  {
+    "revision": "f667c44d7b421ee5fa464d5174ee4d25",
+    "url": "/portfolio-website-3/static/media/Contact2.f667c44d.svg"
+  },
+  {
+    "revision": "214c40523434ae8dd00fcc38f9bf0840",
+    "url": "/portfolio-website-3/static/media/LogoPW2.214c4052.png"
+  },
+  {
+    "revision": "02a6df84cbca947975e8639fc8b5fc82",
+    "url": "/portfolio-website-3/static/media/Profile.02a6df84.svg"
+  },
+  {
+    "revision": "24c401fa76c294198d0acc2504f5b49e",
+    "url": "/portfolio-website-3/static/media/Skills.24c401fa.svg"
+  },
+  {
+    "revision": "37346f8abf8733591ecde4d456f21a13",
+    "url": "/portfolio-website-3/static/media/covid-19-thumb.37346f8a.png"
+  },
+  {
+    "revision": "77993eeb121c03d75ed01fc7dac214f9",
+    "url": "/portfolio-website-3/static/media/portfolio-thumb.77993eeb.png"
+  },
+  {
+    "revision": "68be3641fc24c5f46af4acba7bd47fd6",
+    "url": "/portfolio-website-3/static/media/portfolio-v1-thumb.68be3641.png"
+  }
+]);
